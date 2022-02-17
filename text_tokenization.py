f8345d5e0acec86cec0ad5a6f20d593f59c1fa87
Sayak Paul <spsayakpaul@gmail.com>
2022-02-17 13:15:18 +0530

    initial commit

---
+++ text_tokenization.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import pandas as pd
+
+from typing import Union, Dict, Any, List, Tuple
+from sentence_splitter import split_text_into_sentences
+from ml_collections import ConfigDict
+
+import embeddings as emb
+
+
+def generate_features(example: Union[pd.Series, Dict[str, Any]], config: ConfigDict):
+    """Generates embeddings and labels from the dataset examples.
+    
+    Many of the utilities used in this function were written by Nilabhra
+    Roy Chowdhury (@Nilabhra).
+    """
+    if not hasattr(generate_features, "tokenizer"):
+        generate_features.tokenizer = hub.load(config.preprocessor_path).tokenize
+
+    def _tokenize_text(text: List[str],) -> Tuple[tf.RaggedTensor, List[int]]:
+        """Tokenizes a list of sentences.
+        Args:
+            text (List[str]): A list of sentences.
+        Returns:
+            Tuple[tf.RaggedTensor, List[int]]: Tokenized and indexed sentences, list
+                containing the number of tokens per sentence.
+        """
+        token_list = generate_features.tokenizer(tf.constant(text))
+        token_lens = [tokens.flat_values.shape[-1] for tokens in token_list]
+        return token_list, token_lens
+
+    text_features = {}
+    split_sentences = split_text_into_sentences(example["summary"], language="en")
+
+    text_features["summary"] = example["summary"]
+    tokenized_text_feature, token_lens = _tokenize_text(split_sentences)
+    text_features["summary_tokens"] = tokenized_text_feature
+    text_features["summary_token_lens"] = token_lens
+    text_features["summary_num_sentences"] = len(token_lens)
+
+    text_features["label"] = example["genre"]
+    return text_features
---
+++ tfrecords.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import apache_beam as beam
+
+from typing import Dict, Union
+
+
+def _bytes_feature(bytes_input: bytes) -> tf.train.Feature:
+    """Encodes given data as a byte feature."""
+    bytes_list = tf.train.BytesList(value=[bytes_input])
+    return tf.train.Feature(bytes_list=bytes_list)
+
+
+def _floats_feature(value):
+    """Returns a float_list from a float / double."""
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def create_tfr_example(
+    raw_features: Dict[str, Union[Dict[str, str], tf.Tensor, str]]
+) -> tf.Tensor:
+    """Creates a tf.train.Example instance from high level features."""
+    feature = {
+        "summary": _bytes_feature(raw_features["summary"].encode("utf-8")),
+        "summary_average_embeddings": _floats_feature(
+            raw_features["summary_average_embeddings"].numpy().tolist()
+        ),
+        "label": _bytes_feature(raw_features["label"].encode("utf-8")),
+    }
+    # Wrap as a training example.
+    feature = tf.train.Features(feature=feature)
+    example = tf.train.Example(features=feature)
+    return example
+
+
+class FeaturesToSerializedExampleFn(beam.DoFn):
+    """DoFn class to create a tf.train.Example from high level features."""
+
+    def __init__(self, **kwargs):
+        super()
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def process(self, features):
+        example = create_tfr_example(features)
+        yield example.SerializeToString()
